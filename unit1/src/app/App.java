e9b3d9f75c19b026b2d949052dc88ddf00e26bf5
jameszachman <830518@mystma.org>
2020-09-22 14:24:59 +0000

    Mad Lib Assignment

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,6 +2,24 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        String person1 = "Michael";
+        String verb1 = "code";
+        String dayOfWeek = "Tuesday";
+        String food = "bagel";
+        String adjective1 = "delicious";
+        String verb2 = "eat";
+        String person2 = "John";
+        String place1 = "Casey's";
+        String adjective2 = "new";
+        String place2 = "YMCA";
+        String verb3 = "shoot hoops";
+
+        //[person1] is a programmer who likes to [verb1].
+        System.out.println(person1 + " is a programmer who likes to " + verb1 + ".");
+        //Every [dayOfWeek] they make a [adjective1]  [food] for breakfast and [verb2] at [place1] with [person2].
+        System.out.println("Every " + dayOfWeek + " they make a " + adjective1 + " " + food + " for breakfast and " + verb2 + " at " + place1 + " " + "with" + " " + person2 + ".");
+        //Sometimes, they go to the [adjective2]  [place2] with [person2] so that they can [verb3] together.
+        System.out.println("Sometimes" + " they go to the " + adjective2  + " " + place2 + " with " + person2 + " so that they can " + verb3 + " together.");
+
     }
 }
