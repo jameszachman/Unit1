5927f9f0cfefab3b7045577adb35a3f93d591af9
jameszachman <830518@mystma.org>
2020-09-24 19:49:25 +0000

    User Input

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -6,10 +6,29 @@ public class App {
         
         Scanner input = new Scanner(System.in);
 
+        //FirstName
         System.out.println("Please enter your first name.");
         String name = input.nextLine();
+        
+        //LastName
+        System.out.println("Please enter your last name.");
+        String lastName = input.nextLine();
 
-        System.out.println("Hello, " + name);
+        //Age
+        System.out.println("Please enter your age.");
+        String age = input.nextLine();
+
+        //Grade
+        System.out.println("Please enter your grade level.");
+        String grade = input.nextLine();
+
+        //Color
+
+        System.out.println("Please enter your favorite color.");
+        String color = input.nextLine();
+
+        //FinalMessage
+        System.out.println("Hello, " + name + " " + lastName + ".  You are " + age + " years old, and you are in " + grade + " grade.  Your favorite color is " + color + ".");
         
 
     }
