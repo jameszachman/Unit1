c322325316c988a735041c9ac0fe2cdb4549c713
jameszachman <830518@mystma.org>
2020-09-25 19:41:49 +0000

    While Loops

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,35 +1,35 @@
-package app;
-import java.util.Scanner; 
+package app; 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        Scanner input = new Scanner(System.in);
+        int count = 0;
+        String color = "red";
+        boolean truth = true;
 
-        //FirstName
-        System.out.println("Please enter your first name.");
-        String name = input.nextLine();
-        
-        //LastName
-        System.out.println("Please enter your last name.");
-        String lastName = input.nextLine();
+        //Number Loop
+        while (count <= 10) {
+            System.out.println("The count is " + count);
+            count = count + 1;
+        }
 
-        //Age
-        System.out.println("Please enter your age.");
-        String age = input.nextLine();
+        System.out.println("count finished");
+        //Color Loops
+        while (color == "red") {
+            System.out.println("The color is " + color);
+            color = "green";
+        }
 
-        //Grade
-        System.out.println("Please enter your grade level.");
-        String grade = input.nextLine();
+        System.out.println("Color finished");
 
-        //Color
+        //Boolean Loop
+        while (truth == true) {
+            System.out.println("The statement is " + truth);
+            truth = false;
+        }
 
-        System.out.println("Please enter your favorite color.");
-        String color = input.nextLine();
+        System.out.println("Truth is false");
+    }
 
-        //FinalMessage
-        System.out.println("Hello, " + name + " " + lastName + ".  You are " + age + " years old, and you are in " + grade + " grade.  Your favorite color is " + color + ".");
-        
 
     }
-}
+
