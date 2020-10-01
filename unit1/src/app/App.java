ecea0fd953d64151d596f88f6ba7fd5251cab65b
jameszachman <830518@mystma.org>
2020-10-01 19:39:08 +0000

    If Loops

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,33 +1,18 @@
-package app; 
+package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        for (int i = 10; i <= 20; i = i +1){
-            System.out.println("The count is " + i);
-            System.out.println("Let's keep counting.");
-        
-        }
-        for (int i = 10; i <= 100; i = i +1){
-            System.out.println("The count is " + i);
-            System.out.println("Let's keep counting.");
-        }
-        for (int i = 1000; i <= 256000; i = i +1){
-            System.out.println("The count is " + i);
-            System.out.println("Let's keep counting.");
-        
-        }
-        for (int i = 11; i <= 12; i = i +1){
-            System.out.println("The count is " + i);
-            System.out.println("Let's keep counting.");
-        
-        }
-        for (int i = 10; i >= 0; i = i -1){
-            System.out.println("The count is " + i);
-            System.out.println("Let's keep counting.");
-        
-        }
 
-}
+        for (int i = 1; i <= 20; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i + " is divisible by four");
+            } else {
+                System.out.println(i + " is not divisible by four");
+
+            }
+
+        }
+        System.out.println("All finished");
+    }
 
 }
