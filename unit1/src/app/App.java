846bfb5850a81f9858d8ba79a9819c0d7ae813b1
jameszachman <830518@mystma.org>
2020-10-01 20:17:16 +0000

    If Loops

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,18 +1,76 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i <= 20; i++) {
-            if (i % 4 == 0) {
-                System.out.println(i + " is divisible by four");
-            } else {
-                System.out.println(i + " is not divisible by four");
+        Scanner input = new Scanner(System.in);
+        String answer1;
+        String correctAnswer1 = "Yes" ;
+        String answer2;
+        String correctAnswer2 = "No" ;
+        String answer3;
+        String correctAnswer3 = "16" ;
+        String answer4;
+        String correctAnswer4 = "Water";
+        String answer5;
+        String correctAnswer5 = "Orange";
+        int i = 4;
 
-            }
+        System.out.println("Capitalize your first letter to your answer.");
 
+        //First Question
+        System.out.println("Is the earth round?");
+        answer1 = input.nextLine();
+        if(answer1.equals(correctAnswer1)){
+             System.out.println("You are correct.");
         }
-        System.out.println("All finished");
-    }
+        else{
+            System.out.println("You are incorrect.");
+        }
 
+        //Second Question
+        System.out.println("Is the sky red?");
+        answer2 = input.nextLine();
+        if(answer2.equals(correctAnswer2)){
+             System.out.println("You are correct.");
+        }
+        else{
+            System.out.println("You are incorrect.");
+        }
+
+        //Third Question
+        System.out.println("What is 4 squared?");
+        answer3 = input.nextLine();
+        if(answer3.equals(correctAnswer3)){
+             System.out.println("You are correct.");
+        }
+        else{
+            System.out.println("You are incorrect.");
+        }
+
+        //Fourth Question
+        System.out.println("What is the ocean made out of?");
+        answer4 = input.nextLine();
+        if(answer4.equals(correctAnswer4)){
+             System.out.println("You are correct.");
+        }
+        else{
+            System.out.println("You are incorrect.");
+        }
+
+        //Fifth Question
+        System.out.println("What color is a carrot?");
+        answer5 = input.nextLine();
+        if(answer5.equals(correctAnswer5)){
+             System.out.println("You are correct.");
+        }
+        else{
+            System.out.println("You are incorrect.");
+        }
+
+        System.out.println("Your score is " + i + " out of 5" );
+           
+        }
 }
