3c37534fc7c23c9908eafb5e478cad19d83bef8c
jameszachman <830518@mystma.org>
2020-10-02 19:28:10 +0000

    If Loops Final

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -16,7 +16,8 @@ public class App {
         String correctAnswer4 = "Water";
         String answer5;
         String correctAnswer5 = "Orange";
-        int i = 4;
+        int i = 0;
+        int p = 0;
 
         System.out.println("Capitalize your first letter to your answer.");
 
@@ -25,9 +26,13 @@ public class App {
         answer1 = input.nextLine();
         if(answer1.equals(correctAnswer1)){
              System.out.println("You are correct.");
+             i = i + 1; 
+             p = p + 20;
         }
         else{
             System.out.println("You are incorrect.");
+            i = i + 0;
+            p = p + 0;
         }
 
         //Second Question
@@ -35,19 +40,27 @@ public class App {
         answer2 = input.nextLine();
         if(answer2.equals(correctAnswer2)){
              System.out.println("You are correct.");
+             i = i + 1;
+             p = p + 20;
         }
         else{
             System.out.println("You are incorrect.");
+            i = i + 0;
+            p = p + 0;
         }
 
         //Third Question
-        System.out.println("What is 4 squared?");
+        System.out.println("What is 4 squared? (PLEASE USE DIGITS FOR YOUR ANSWER)");
         answer3 = input.nextLine();
         if(answer3.equals(correctAnswer3)){
              System.out.println("You are correct.");
+             i = i + 1;
+             p = p + 20;
         }
         else{
             System.out.println("You are incorrect.");
+            i = i + 0;
+            p = p + 0;
         }
 
         //Fourth Question
@@ -55,9 +68,13 @@ public class App {
         answer4 = input.nextLine();
         if(answer4.equals(correctAnswer4)){
              System.out.println("You are correct.");
+             i = i + 1;
+             p = p + 20;
         }
         else{
             System.out.println("You are incorrect.");
+            i = i + 0;
+            p = p + 0;
         }
 
         //Fifth Question
@@ -65,12 +82,17 @@ public class App {
         answer5 = input.nextLine();
         if(answer5.equals(correctAnswer5)){
              System.out.println("You are correct.");
+             i = i + 1;
+             p = p + 20;
         }
         else{
             System.out.println("You are incorrect.");
+            i = i + 0;
+            p = p + 0;
         }
 
         System.out.println("Your score is " + i + " out of 5" );
+        System.out.println("You got " + p + "%.");
            
         }
 }
