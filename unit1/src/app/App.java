c30f7593a9b6174bd149011d8d79172a8f3da4e6
jameszachman <830518@mystma.org>
2020-10-07 14:33:41 +0000

    Axolotl Facts

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -6,188 +6,64 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
-        String answer1;
-        String correctAnswer1 = "Yes" ;
-        String answer2;
-        String correctAnswer2 = "No" ;
-        String answer3;
-        String correctAnswer3 = "16" ;
-        String answer4;
-        String correctAnswer4 = "Water";
-        String answer5;
-        String correctAnswer5 = "Orange";
-        int i = 0;
-        int p = 0;
 
-        System.out.println("Capitalize your first letter to your answer.");
-
-        //First Question
-        System.out.println("Is the earth round?");
-        answer1 = input.nextLine();
-        if(answer1.equals(correctAnswer1)){
-             System.out.println("You are correct.");
-             i = i + 1; 
-             p = p + 10;
+        System.out.println("Welcome to Axolotl facts! Please select a number between 1-5.");
+        //First Fact
+        int answer1 = input.nextInt();
+        if(answer1 == 1){
+             System.out.println("Axolotls are a group of amphibians that thrive in the waterways of Xochimilco, Mexico.");
+             
         }
         else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
-        }
-
-        //Second Question
-        System.out.println("Is the sky red?");
-        answer2 = input.nextLine();
-        if(answer2.equals(correctAnswer2)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
-        }
-        else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
-        }
-
-        //Third Question
-        System.out.println("What is 4 squared? (PLEASE USE DIGITS FOR YOUR ANSWER)");
-        answer3 = input.nextLine();
-        if(answer3.equals(correctAnswer3)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
-        }
-        else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
-        }
-
-        //Fourth Question
-        System.out.println("What is the ocean made out of?");
-        answer4 = input.nextLine();
-        if(answer4.equals(correctAnswer4)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
-        }
-        else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
-        }
-
-        //Fifth Question
-        System.out.println("What color is a carrot?");
-        answer5 = input.nextLine();
-        if(answer5.equals(correctAnswer5)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
-        }
-        else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
-        }
-
-        System.out.println("Your score is " + i + " out of 5" );
-        System.out.println("You got " + p + "%.");
-           
-        }
-
-        //Sixth Question
-        System.out.println("What color is a carrot?");
-        answer5 = input.nextLine();
-        if(answer5.equals(correctAnswer5)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
-        }
-        else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
-        }
-
-        System.out.println("Your score is " + i + " out of 5" );
-        System.out.println("You got " + p + "%.");
-           
-        }
-
-        //Seventh Question
-        System.out.println("What color is a carrot?");
-        answer5 = input.nextLine();
-        if(answer5.equals(correctAnswer5)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
-        }
-        else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
-        }
-
-        System.out.println("Your score is " + i + " out of 5" );
-        System.out.println("You got " + p + "%.");
-           
+            System.out.println("You haven't selected a number between 1-5.");
+            
         }
         
-        //Eighth Question
-        System.out.println("What color is a carrot?");
-        answer5 = input.nextLine();
-        if(answer5.equals(correctAnswer5)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
+        //Second Fact
+        int answer2 = input.nextInt();
+        if(answer2 == 2){
+             System.out.println("They have a set of feathery branches on the sides of their head that work as their gills.");
+             
         }
         else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
+            System.out.println("You haven't selected a number between 1-5.");
+            
         }
 
-        System.out.println("Your score is " + i + " out of 5" );
-        System.out.println("You got " + p + "%.");
-           
-        }
-        
-        //Ninth Question
-        System.out.println("What color is a carrot?");
-        answer5 = input.nextLine();
-        if(answer5.equals(correctAnswer5)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
+        //Third Fact
+        int answer3 = input.nextInt();
+        if(answer3 == 3){
+             System.out.println("Axolotls are a critically endangered species that thrives in capitivity more than in the wild.");
+             
         }
         else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
+            System.out.println("You haven't selected a number between 1-5.");
+            
         }
 
-        System.out.println("Your score is " + i + " out of 5" );
-        System.out.println("You got " + p + "%.");
-           
-        }
-        
-        //Tenth Question
-        System.out.println("What color is a carrot?");
-        answer5 = input.nextLine();
-        if(answer5.equals(correctAnswer5)){
-             System.out.println("You are correct.");
-             i = i + 1;
-             p = p + 10;
+        //Fourth Fact
+        int answer4 = input.nextInt();
+        if(answer4 == 4){
+             System.out.println("Axolotls have a mythological story about the Aztec god of all things grim, Xolotl, who was a dog-headed being that was fearful of being killed.  It is believed by some that he transformed into an axolotl to hide, but cannot transform back to his true form.");
+             
         }
         else{
-            System.out.println("You are incorrect.");
-            i = i + 0;
-            p = p + 0;
+            System.out.println("You haven't selected a number between 1-5.");
+            
         }
 
-        System.out.println("Your score is " + i + " out of 5" );
-        System.out.println("You got " + p + "%.");
+        //Fifth Fact
+        int answer5 = input.nextInt();
+        if(answer5 == 5){
+             System.out.println("Axolotls have a regenerative ability that allows them to grow back any limb, their spines, their jaws, and even their brain.");
+             
+        }
+        else{
+            System.out.println("You haven't selected a number between 1-5.");
+            
+        }
+
+       input.close();
            
         }
 }
