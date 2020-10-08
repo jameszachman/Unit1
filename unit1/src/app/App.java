b8fa20199da8ae2c1393607bdd5e0476dd7aa249
jameszachman <830518@mystma.org>
2020-10-08 19:56:24 +0000

    Challenge 2

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -4,66 +4,26 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+    Scanner input = new Scanner(System.in);
+        int i = 0;
+    String password = input.next();
 
-        Scanner input = new Scanner(System.in);
+    System.out.println("Please enter password.");
 
-        System.out.println("Welcome to Axolotl facts! Please select a number between 1-5.");
-        //First Fact
-        int answer1 = input.nextInt();
-        if(answer1 == 1){
-             System.out.println("Axolotls are a group of amphibians that thrive in the waterways of Xochimilco, Mexico.");
-             
+    //Questions
+        if(password == "lordoftherings"){
+            System.out.println("You have found the password.");
         }
-        else{
-            System.out.println("You haven't selected a number between 1-5.");
-            
+    //Wrong Answer
+        if(password != "lordoftherings"){
+            System.out.println("Wrong password.  Try again.");
+            i = i + 1;
         }
-        
-        //Second Fact
-        int answer2 = input.nextInt();
-        if(answer2 == 2){
-             System.out.println("They have a set of feathery branches on the sides of their head that work as their gills.");
-             
+    //Lock Out
+        if(i == 3){
+            System.out.println("Too many attempts.  Relaunch program to try again.");
+            System.exit(1);
         }
-        else{
-            System.out.println("You haven't selected a number between 1-5.");
-            
-        }
-
-        //Third Fact
-        int answer3 = input.nextInt();
-        if(answer3 == 3){
-             System.out.println("Axolotls are a critically endangered species that thrives in capitivity more than in the wild.");
-             
-        }
-        else{
-            System.out.println("You haven't selected a number between 1-5.");
-            
-        }
-
-        //Fourth Fact
-        int answer4 = input.nextInt();
-        if(answer4 == 4){
-             System.out.println("Axolotls have a mythological story about the Aztec god of all things grim, Xolotl, who was a dog-headed being that was fearful of being killed.  It is believed by some that he transformed into an axolotl to hide, but cannot transform back to his true form.");
-             
-        }
-        else{
-            System.out.println("You haven't selected a number between 1-5.");
-            
-        }
-
-        //Fifth Fact
-        int answer5 = input.nextInt();
-        if(answer5 == 5){
-             System.out.println("Axolotls have a regenerative ability that allows them to grow back any limb, their spines, their jaws, and even their brain.");
-             
-        }
-        else{
-            System.out.println("You haven't selected a number between 1-5.");
-            
-        }
-
-       input.close();
-           
-        }
+        input.close();
+    }
 }
