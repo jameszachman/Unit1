62b550e7588d35bfe31773b81e8d3534805df60f
jameszachman <830518@mystma.org>
2020-10-09 02:58:37 +0000

    Challenge 2

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -11,21 +11,47 @@ public class App {
     String pattern = input.nextLine();
 
     //Questions
-
+    while(i != 3){
         if(pattern.equals("lordoftherings") ){
             System.out.println("You have found the password.");
             System.exit(1);
         }
         else{
         System.out.println("Wrong password.  Try again.");
-            i = i + 1;
-        System.out.println("Please enter password.");  
+        i = i + 1;  
+        }
+        
+        System.out.println("Please enter password.");   
+        String pattern2 = input.nextLine(); 
+
+        if(pattern2.equals("lordoftherings") ){
+            System.out.println("You have found the password.");
+            System.exit(1);
+        }
+        else{
+        System.out.println("Wrong password.  Try again.");
+        i = i + 1;
         }
 
-        if(i == 3){
+        System.out.println("Please enter password.");
+        String pattern3 = input.nextLine(); 
+
+        if(pattern3.equals("lordoftherings") ){
+            System.out.println("You have found the password.");
+            System.exit(1);
+        }
+        else{
+        System.out.println("Wrong password.  Try again.");
+        i = i + 1;
+
+        System.out.println("Please enter password.");      
+        }
+        
+    }
+    if(i == 3){
             System.out.println("Too many attempts.  Relaunch program to try again.");
             System.exit(1);
         }
         input.close();
-    }
+}
 }
