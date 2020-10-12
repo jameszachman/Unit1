4b0086bd251d028adbe017f52eddd8a09489e580
jameszachman <830518@mystma.org>
2020-10-12 16:58:52 +0000

    Challenge 3

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,53 +5,31 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
     Scanner input = new Scanner(System.in);
-        
-    System.out.println("Please enter password.");
-    int i = 0;
-    String pattern = input.nextLine();
+    int i = 36;
+    boolean doneFinish = false;
 
-    //Questions
-    while(i != 3){
-        if(pattern.equals("lordoftherings") ){
-            System.out.println("You have found the password.");
-            System.exit(1);
-        }
-        else{
-        System.out.println("Wrong password.  Try again.");
-        i = i + 1;  
-        }
-        
-        System.out.println("Please enter password.");   
-        String pattern2 = input.nextLine(); 
+    System.out.println("Pick a number between 0-100.");
+    input.nextLine();
 
-        if(pattern2.equals("lordoftherings") ){
-            System.out.println("You have found the password.");
-            System.exit(1);
-        }
-        else{
-        System.out.println("Wrong password.  Try again.");
-        i = i + 1;
-        }
+    while(doneFinish == false){
 
-        System.out.println("Please enter password.");
-        String pattern3 = input.nextLine(); 
-
-        if(pattern3.equals("lordoftherings") ){
-            System.out.println("You have found the password.");
-            System.exit(1);
-        }
-        else{
-        System.out.println("Wrong password.  Try again.");
-        i = i + 1;
-
-        System.out.println("Please enter password.");      
-        }
-        
+    if(input.nextInt() == i){
+        System.out.println("You have found the number!");
+        doneFinish = true;
+       
     }
-    if(i == 3){
-            System.out.println("Too many attempts.  Relaunch program to try again.");
-            System.exit(1);
+    else{
+        if(input.nextInt() < i){
+                System.out.println("Too low.");
+                
+            }
+            else{
+                if(input.nextInt() > i){
+                        System.out.println("Too high");
+            }
+            }
         }
+    }
         input.close();
 }
 }
