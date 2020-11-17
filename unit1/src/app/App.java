384c7a733fa44ba4710836d2e762b523f37afd1d
jameszachman <830518@mystma.org>
2020-11-17 17:06:12 +0000

    Roulette Challenge

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -15,108 +15,124 @@ public class App {
     System.out.println("Would you like to make an inside or outside bet?");
     String response = input.nextLine();
 
-    if(response == "inside"){
+    if(response.equals("inside")){
         System.out.println("Which type of bet would you like? (straight-up, split, street, corner, five or line)");
+        input.nextLine();
 
         //Straight-Up Bets
-        if(response == "straight-up"){
+        if(response.equals("straight-up")){
             System.out.println("What is the number you want to bet on?");
+            input.nextInt();
         }
 
         //Split Bets
-        if(response == "split"){
+        if(response.equals("split")){
             System.out.println("What are the numbers you want to split? (Choose two numbers that are adjacent to each other. (Ex: 15-16, 20-21))");
+            input.nextInt();
         }
 
         //Street Bets
-        if(response == "street"){
+        if(response.equals("street")){
             System.out.println("What are the three numbers you want to bet on? (Choose three numbers that are in line with each other. (Ex: 7-8-9, 13-14-15))");
+            input.nextInt();
         }
 
         //Corner Bets
-        if(response == "corner"){
+        if(response.equals("corner")){
             System.out.println("What are the four numbers you want to bet on?  (Choose four numbers that form a square together.  (Ex: 1-2-4-5, 16-17-19-20))");
+            input.nextInt();
         }
 
         //Five Bets
-        if(response == "five"){
+        if(response.equals("five")){
             System.out.println("You have chosen to bet on 0, 00, 1, 2 and 3.  Now spinning...");
+            input.nextInt();
         }
 
         //Line Bets
-        if(response == "line"){
+        if(response.equals("line")){
             System.out.println("What are the six numbers you want to bet on?  (Choose six number that form a rectangle on the board starting from the left edge of the board and ending on the right edge.  (Ex: 22-27, 10-15))");
+            input.nextInt();
         }
     }
 
     //Outside Bets
-    if(response == "outside"){
+    if(response.equals("outside")){
         System.out.println("Which type of bet would you like? (column, dozen, color, odd or even, low or high)");
+        input.nextLine();
 
         //Column Bets
-        if(response == "column"){
+        if(response.equals("column")){
             System.out.println("Which column would you like? 1st, 2nd or 3rd?");
+            input.nextLine();
 
-            if(response == "1st"){
+            if(response.equals("1st")){
             System.out.println("You've chosen the 1st column.  Now spinning...");
             }
 
-            if(response == "2nd"){
+            if(response.equals("2nd")){
             System.out.println("You've chosen the 2nd column.  Now spinning...");
             }
 
-            if(response == "3rd"){
+            if(response.equals("3rd")){
             System.out.println("You've chosen the 3rd column.  Now spinning...");
             }
         }
         
         //Dozen Bets
-        if(response == "dozen"){
+        if(response.equals("dozen")){
             System.out.println("Which dozen would you like? 1st 12, 2nd 12 or 3rd 12?");
+            input.nextLine();
 
-            if(response == "1st 12"){
+            if(response.equals("1st 12")){
                 System.out.println("You've chosen the 1st 12.  Now spinning...");
             }
 
-            if(response == "2nd 12"){
+            if(response.equals("2nd 12")){
                 System.out.println("You've chosen the 2nd 12.  Now spinning...");
             }
 
-            if(response == "3rd 12"){
+            if(response.equals("3rd 12")){
                 System.out.println("You've chosen the 3rd 12.  Now spinning...");
             }
 
         //Bet on Color
-        if(response == "color"){
+        if(response.equals("color")){
             System.out.println("Which color would you like to bet on? Black or red?");
+            input.nextLine();
 
-            if(response == "black"){
+            if(response.equals("black")){
                 System.out.println("You've chosen black.  Now spinning...");
             }
 
-            if(response == "red"){
+            if(response.equals("red")){
                 System.out.println("You've chosen red.  Now spinning...");
             }
         }
 
         //Bet on Odd
-        if(response == "odd"){
+        if(response.equals("odd")){
             System.out.println("You've chosen to bet on all odd numbers.  Now spinning...");
+            input.nextLine();
         }
 
         //Bet on Even
-        if(response == "even"){
+        if(response.equals("even")){
             System.out.println("You've chosen to bet on all even numbers.  Now spinning...");
+            input.nextLine();
         }
+        
 
         //Bet on Low
-        if(response == "low"){
+        if(response.equals("low")){
             System.out.println("You've chosen to bet on the low numbers.  Now spinning...");
+            input.nextLine();
         }
 
         //Bet on High
-        if(response == "high"){
+        if(response.equals("high")){
             System.out.println("You've chosen to bet on the high numbers.  Now spinning...");
+            input.nextLine();
         }
 
         }
